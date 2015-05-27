e73a7c1ab30eb9ae2ee30d65980335083e2d1ab8
Page <michel_page38@yahoo.fr>
2015-05-27 18:31:55 +0200

    MP 2015-05-27 18:30 - Initial commit of R+markdown script for data preparation (incomplete). Rmd script is meant to be run using knit.

--- titanic/R/script.R
+++ titanic/R/script.R
@@ -1,1 +1,37 @@
+setwd("/github/kaggle-fiddling/titanic/R")
 
+library(ggplot2)
+
+```
+
+Let us first define a generic function for reading CSV files which handles data types and missing values
+
+```{r, message=F, warning=F}
+
+# file.path: path of CSV file to be read
+# column.types:  type of each column
+# missing.types: string corresponding to missing data
+readCsvData <- function(file.path, column.types, missing.types) {
+  read.csv(file.path, colClasses=column.types, na.strings=missing.types)
+}
+
+missing.types <- c("NA", "")
+train.column.types <- c('integer',   # PassengerId
+                        'factor',    # Survived 
+                        'factor',    # Pclass
+                        'character', # Name
+                        'factor',    # Sex
+                        'numeric',   # Age
+                        'integer',   # SibSp
+                        'integer',   # Parch
+                        'character', # Ticket
+                        'numeric',   # Fare
+                        'character', # Cabin
+                        'factor'     # Embarked
+)
+train <- readCsvData("../data/train.csv", train.column.types, missing.types)
+
+
+barplot(prop.table(train$Survived),
+        names.arg = c("Perished", "Survived"),
+        main="Survived (passenger fate)", col="black")
